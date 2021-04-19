31acf12ec9bed34e248fa17076444dbb57acd67d
thrassoz <thrassosoikonomou@gmail.com>
2021-04-19 18:05:49 +0300

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 import time
 
 def products_by_category():
-
+    print("Insert a name to search for: ")
     name = input()
+    print("Loading...")
     browser = webdriver.Chrome()
     browser.get('https://www.toys-shop.gr/')
 
@@ -28,13 +29,20 @@ def products_by_category():
     d = {
 
     }
+    time.sleep(5)
+    while True:
+        products = browser.find_elements_by_class_name('product-description')
+        for i in range(len(products)):
+            description = products[i].text.rsplit('\n')
+            d[f'{description[3]}'] = f'{description[4]}' #change the format of the dict
 
-    time.sleep(2)
-    products = browser.find_elements_by_class_name('product-description')
-    for i in range(len(products)):
-        description = products[i].text.rsplit('\n')
-        d[f'{description[3]}'] = f'{description[4]}'
-
+        try:
+            next_page = browser.find_element_by_id('infinity-url')
+        except:
+            print("No more pages")
+            break
+        next_page.click()
+        time.sleep(5)
     return d
 
 catalogue = products_by_category()
